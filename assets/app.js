90370fd373abcb69c61fd4efa1c7689e2a25d86c
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-08-23 11:33:39 -0300

    fixed:add basic functions

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,57 @@
 'use strict'
 
-const GameBoard = (function(){
-    const gameBoard = [];
+const Player = () => {  
+    let option  
+    const getOption = () => {     
+        document.querySelector('.btns').addEventListener('click',e => {
+            option = e.target;
+            option.setAttribute('disabled','')
+            return option.value
+        });
+    }
 
-    return gameBoard
+    return{ getOption }
+
+}   
+
+const GameBoard = (function(){
+    
+    let cells = document.querySelectorAll(".cell");
+    
+    const gameBoard = [];
+    
+    let option1 = Player() //instance of the function factory Player 
+    option1 = option1.getOption()
+   
+    function addMark(){
+      cells.forEach((cell) => {
+        cell.addEventListener("click",e => {
+          console.log(option1)
+          e.target.innerText = option1
+        });
+      })  
+    }
+
+    function fillBoard(){
+        cells.forEach((cell)=>{
+            gameBoard.push(cell.innerText)
+        }); 
+        return gameBoard
+    }
+  
+    return {fillBoard,addMark,option1}  
 
 })();
 
 const gameDisplay = (function(){
-
+  
+   
+      
 })();
 
-const Player = (option) => {
-    
 
-}   
+
+ window.addEventListener('DOMContentLoaded',e => {
+    GameBoard.addMark()
+    GameBoard.option1
+}) 

76492a588c03e91f5c310cb16e96ab6588cbd027
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-08-24 20:32:56 -0300

    fixed: add functions reviewArray and winnerIs
    BREAKING CHANGE: add funcitonality to updateScreen

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const Player = (player, marker) => {
   const getMarker = () => marker;
   const getPlayer = () => player;
@@ -8,31 +6,56 @@ const Player = (player, marker) => {
 
 const displayController = (function () {})();
 
+const GameBoard = (function () {
+  let cells = document.querySelectorAll(".cell");
+  const gameBoard = [];
+
+  const fillBoard = () => {
+    cells.forEach((cell) => {
+      gameBoard.push(cell.innerText);
+    });
+    return gameBoard;
+  };
+
+  const addMarker = (marker) => {
+    cells.forEach((cell) =>
+      cell.addEventListener("click", (e) => {
+        e.target.value = marker;
+        e.target.disabled = "";
+        gameBoard.push(e.target.value)
+      })
+    );
+  };
+
+  return { gameBoard, addMarker };
+})();
+
 const logicalGame = (function () {
   let indexArrayO = [],
     indexArrayX = [];
 
-  const takeIndex = () => {
+  const takeIndex = (gameBoard) => {
     gameBoard.map((e, i) => {
       e === "x" ? indexArrayX.push(i) : indexArrayO.push(i);
     });
+    return { indexArrayO, indexArrayX };
   };
 
   const winOptions = [
-    ["0", "1", "2"],
-    ["3", "4", "5"],
-    ["6", "7", "8"],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
 
-    ["0", "3", "6"],
-    ["1", "4", "7"],
-    ["2", "5", "8"],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
 
-    ["0", "4", "8"],
-    ["2", "4", "6"],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
 
   const reviewArray = (arr) => {
-    arr = arr.sort();
+    let res;
     for (let el of winOptions) {
       res = el.every((n) => arr.includes(n));
       if (res) {
@@ -42,26 +65,40 @@ const logicalGame = (function () {
     return res;
   };
 
-  return {};
+  const winnerIs = (arr) => {
+    let player = arr === playerO ? "O" : "X";
+    if (arr) {
+      return `the winner is ${player}`;
+    } else {
+      return `you lose ${player}`;
+    }
+  };
 
+  return { takeIndex, reviewArray, winnerIs };
 })();
 
-const GameBoard = (function () {
-  let cells = document.querySelectorAll(".cell");
-  const gameBoard = [];
+const updateScreen = (function () {
+  let player1 = Player("pepe", "x"),
+    marker = player1.getMarker(),
+    name = player1.getPlayer();
 
-  function fillBoard() {
-    cells.forEach((cell) => {
-      gameBoard.push(cell.innerText);
-    });
-    return gameBoard;
+  if (GameBoard.gameBoard.length === 0) {
+    console.log("entro al if true");
+
+    GameBoard.addMarker(marker);
+  
   }
-
-  return { gameBoard };
+  return { marker, name };
 })();
 
-const updateScreen = (function () {})();
+//prueba
+let boardArr = GameBoard.gameBoard;
+const positionArr = logicalGame.takeIndex(boardArr);
 
-window.addEventListener("DOMContentLoaded", (e) => {
-  console.log(GameBoard.gameBoard);
-});
+let playerX = logicalGame.reviewArray(positionArr.indexArrayX);
+let playerO = logicalGame.reviewArray(positionArr.indexArrayO);
+
+/* console.log(logicalGame.winnerIs(playerO));
+console.log(logicalGame.winnerIs(playerX));
+ */
+updateScreen;
